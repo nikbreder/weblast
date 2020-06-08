f8dba3ce0f47dd175cfc74cff5af8b5009570e47
nikbreder <64217083+nikbreder@users.noreply.github.com>
2020-06-09 00:39:02 +0600

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404</title>
+
+    <style>
+        body {
+            font-family: sans-serif;
+
+        }
+
+        h1 {
+            color: rgb(60, 141, 188);
+
+        }
+
+        a {
+            width: 200px;
+            padding: 15px 5px;
+            text-transform: uppercase;
+            vertical-align: middle;
+            background: rgb(60, 141, 188);
+            border: none;
+            color: #ffffff;
+            font-weight: 600;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgb(54, 127, 169);
+            text-decoration: none;
+            text-align: center;
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    <h1>Ошибка 404. Страница не найдена.<br> Нажмите на кнопку чтобы вернуться назад.</h1>
+
+    <a href="http://schedule">Назад</a>
+</body>
+</html>
